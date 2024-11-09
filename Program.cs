299c15ec788865aab4812c5281a1215d1ab4dc60
ender32112 <73356846+ender32112@users.noreply.github.com>
2024-11-09 12:37:41 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace GuessTheNumber
+{
+    public class GameLogic
+    {
+        public static int PlayGame(int maxNumber, int maxAttempts)
+        {
+            Random random = new Random();
+            int secretNumber = random.Next(1, maxNumber + 1);
+            int attempts = 0;
+            int guess;
+
+            Console.WriteLine($"Угадайте число от 1 до {maxNumber}. У вас {maxAttempts} попыток.");
+
+            do
+            {
+                attempts++;
+                Console.Write($"Попытка {attempts}: Введите число: ");
+                if (int.TryParse(Console.ReadLine(), out guess))
+                {
+                    if (guess < secretNumber)
+                    {
+                        Console.WriteLine("Слишком мало!");
+                    }
+                    else if (guess > secretNumber)
+                    {
+                        Console.WriteLine("Слишком много!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Вы угадали! Число было {secretNumber}. Вам потребовалось {attempts} попыток.");
+                        return attempts;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод. Попробуйте ещё раз.");
+                    attempts--; // Не учитывать некорректный ввод как попытку
+                }
+            } while (attempts < maxAttempts);
+
+            Console.WriteLine($"Вы не угадали. Число было {secretNumber}.");
+            return -1; // -1 обозначает проигрыш
+        }
+    }
+}
