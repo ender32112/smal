9db8e25632b81a9f8b480e87a0b3f7b6ca2f71fd
ender32112 <73356846+ender32112@users.noreply.github.com>
2024-11-09 12:40:23 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GuessTheNumber
+{
+    public class Authentication
+    {
+        private static Dictionary<string, List<int>> userScores = new Dictionary<string, List<int>>();
+
+        public static bool RegisterUser(string username)
+        {
+            if (userScores.ContainsKey(username))
+            {
+                Console.WriteLine("Пользователь с таким именем уже существует.");
+                return false;
+            }
+            userScores.Add(username, new List<int>());
+            Console.WriteLine($"Пользователь {username} успешно зарегистрирован.");
+            return true;
+        }
+
+        public static void SaveScore(string username, int score)
+        {
+            userScores[username].Add(score);
+        }
+
+        public static void ShowScores(string username)
+        {
+            if (userScores.ContainsKey(username))
+            {
+                Console.WriteLine($"Результаты пользователя {username}:");
+                if (userScores[username].Count > 0)
+                {
+                    foreach (int score in userScores[username])
+                    {
+                        Console.WriteLine(score == -1 ? "Проигрыш" : $"Количество попыток: {score}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Нет сохраненных результатов.");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Пользователь {username} не найден.");
+            }
+        }
+    }
+}
+
