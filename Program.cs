f522ca3ec3afb0e6514d787ee504fa64f4e4f7a3
ender32112 <73356846+ender32112@users.noreply.github.com>
2024-11-15 17:25:07 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GuessTheNumber
+{
+    public class Login
+    {
+        public static bool AuthenticateUser(string username, Dictionary<string, List<int>> userScores)
+        {
+            if (userScores.ContainsKey(username))
+            {
+                Console.WriteLine($"Добро пожаловать, {username}!");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"Пользователь с именем '{username}' не найден.");
+                return false;
+            }
+        }
+    }
+}
