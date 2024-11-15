66098394f59d29b9cd7fbb066327abdc15b7af53
ender32112 <73356846+ender32112@users.noreply.github.com>
2024-11-15 17:24:24 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GuessTheNumber
+{
+    public class Registration
+    {
+        public static bool RegisterNewUser(string username, Dictionary<string, List<int>> userScores)
+        {
+            if (userScores.ContainsKey(username))
+            {
+                Console.WriteLine($"Пользователь с именем '{username}' уже существует.");
+                return false;
+            }
+            userScores.Add(username, new List<int>());
+            Console.WriteLine($"Пользователь '{username}' успешно зарегистрирован.");
+            return true;
+        }
+    }
+}
